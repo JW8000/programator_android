a868129a86b78fef045698d708f795d4539cf0e3
JW8000 <kuba.wojewodzic00@gmail.com>
2023-09-15 18:19:40 +0200

    Comments resolved

--- lib/common/constans/app_colors.dart
+++ lib/common/constans/app_colors.dart
@@ -3,22 +3,21 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color background = Color(0xFF776A87);
 
-  //TODO: popraw reszte z 00xxxxxx na FFxxxxxxx (inaczej nie zadziała)
-  static const Color primary = Colors.blue; //TODO add primary color
+  static const Color primary = Color(0XFF776A87);
 
-  static const Color text = Color(0x00FFFFFF);
+  static const Color text = Color(0xFFFFFFFF);
 
-  static final Color button = const Color(0x00000000).withOpacity(0.75); //tu final
+  static final Color button = const Color(0xFF000000).withOpacity(0.75); //tu final
 
-  static const Color border = Color(0x00000000);
+  static const Color border = Color(0xFF000000);
 
-  static const Color header = Color(0x002C2C2E);
+  static const Color header = Color(0xFF2C2C2E);
 
-  static const Color icon = Color(0x00FFFFFF);
+  static const Color icon = Color(0xFFFFFFFF);
 
-  static const Color appBar = Color(0x00FFFFFF);
+  static const Color appBar = Color(0xFFFFFFFF);
 
-  static const Color appBarIcon = Color(0x00000000);
+  static const Color appBarIcon = Color(0xFF000000);
 
-  static final Color hint = const Color(0x00FFFFFF).withOpacity(0.50); //i tu też final
+  static final Color hint = const Color(0xFFFFFFFF).withOpacity(0.50); //i tu też final
 }
--- lib/common/constans/app_text.dart
+++ lib/common/constans/app_text.dart
@@ -1,39 +1,37 @@
 import 'package:flutter/material.dart';
-
 import 'app_colors.dart';
 
 class AppText {
-  //TODO: znajdź sobie jakiś font na necie i zrób tym fontem to z różnymi Weight
   static TextStyle hint = TextStyle(
-    fontFamily: 'ABeeZee',
+    fontFamily: 'Rubik',
     fontSize: 15,
     fontWeight: FontWeight.w400,
     color: AppColors.hint,
   );
 
   static const TextStyle title = TextStyle(
-    fontFamily: 'ABeeZee',
+    fontFamily: 'Rubik',
     fontSize: 18,
     fontWeight: FontWeight.w400,
     color: AppColors.text,
   );
 
   static const TextStyle textVerySmall = TextStyle(
-    fontFamily: 'ABeeZee',
+    fontFamily: 'Rubik',
     fontSize: 13,
     fontWeight: FontWeight.w400,
     color: AppColors.text,
   );
 
   static const TextStyle button = TextStyle(
-    fontFamily: 'ABeeZee',
+    fontFamily: 'Rubik',
     fontSize: 18,
     fontWeight: FontWeight.w400,
     color: AppColors.text,
   );
 
   static const TextStyle header = TextStyle(
-    fontFamily: 'ABeeZee',
+    fontFamily: 'Rubik',
     fontSize: 21,
     fontWeight: FontWeight.w400,
     color: AppColors.header,
